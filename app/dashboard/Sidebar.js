62a13cfe198c52258f2db274cd6d4fdcc4d1cc48
p-mcgrath <104158869+p-mcgrath@users.noreply.github.com>
2022-11-30 17:56:03 -0500

    initial view

--- app/dashboard/Sidebar.js
+++ app/dashboard/Sidebar.js
@@ -1,10 +1,27 @@
-"use client"
-import React from 'react'
 
-const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
-
-export default Sidebar
+import React from "react";
+ 
+async function fetchRecipients() {
+    const res = await fetch('/api/recipients')
+    return res.json()
+    //return [];
+};
+ 
+async function Sidebar() {
+ const recipients = await fetchRecipients();
+ return (
+   <div className="fixed left-4 h-[96vh] w-[20%] p-4">
+     <div className="flex w-full h-full rounded-md border-2 border-black bg-blue-50">
+       <div className="flex w-full justify-center items-between">
+         <div className="w-full">
+           <h3 className="text-center border-b-2 border-black">
+             Gift Recipients
+           </h3>
+         </div>
+       </div>
+     </div>
+   </div>
+ );
+};
+ 
+export default Sidebar;
