881253344e9fb78bddaad2f25dee92ff55e6c1d4
p-mcgrath <104158869+p-mcgrath@users.noreply.github.com>
2022-12-08 11:52:36 -0500

    dashboard layout

--- app/dashboard/[recipient]/layout.js
+++ app/dashboard/[recipient]/layout.js
@@ -1,15 +1,18 @@
-"use client"
+"use client";
 import UserInfo from "./UserInfo";
 import NavigationTabs from "./NavigationTabs";
 
 function RecipientLayout({ children, params }) {
   return (
-    <div className="">
-        <UserInfo />
-        <NavigationTabs params={params} />
+    <div className="flex flex-col justify-start w-full h-full">
+      <UserInfo />
+      <NavigationTabs params={params} />
+      <div className="flex flex-col justify-start w-full h-full bg-cadeau-50 rounded-b-md shadow-xl">
         {children}
+      </div>
     </div>
   );
 }
 
 export default RecipientLayout;
+
