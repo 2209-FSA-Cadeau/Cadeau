89319d54659ec7c1d03242384f0ffb6c6a09dde4
p-mcgrath <104158869+p-mcgrath@users.noreply.github.com>
2022-12-08 11:51:08 -0500

    notes page

--- app/dashboard/[recipient]/notes/page.js
+++ app/dashboard/[recipient]/notes/page.js
@@ -1,11 +1,44 @@
+"use client";
 import React from "react";
-import Editor from "./Editor";
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import "react-quill/dist/quill.snow.css";
 
 const page = () => {
+  const [value, setValue] = useState("");
+  const [updated, setUpdated] = useState(true);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      setUpdated(false);
+      console.log("send to back end", `${value}`);
+      setTimeout(() => {
+        setUpdated(true);
+      }, 2000);
+    }, 2000);
+    return () => clearTimeout(delayDebounceFn);
+  }, [value]);
+
   return (
-    <div>
-      <div>Notes</div>
-      <Editor />
+    <div className="flex flex-col justify-start w-full h-full">
+      <div className="flex justify-between h-[50px] border-b-2 border-cadeau-900 my-4">
+        <h1 className="mx-4">Notes</h1>
+        <div className="mr-6">
+          {!updated ? "Saving..." : null}
+        </div>
+      </div>
+      <div className="flex flex-col justify-start w-full h-full">
+        <div className="basis-[100%]">
+          <ReactQuill
+            value={value}
+            onChange={setValue}
+            placeholder="Add some ideas!"
+            className="flex flex-col h-full rounded-sm text-cadeau-900"
+          />
+        </div>
+       
+      </div>
     </div>
   );
 };
