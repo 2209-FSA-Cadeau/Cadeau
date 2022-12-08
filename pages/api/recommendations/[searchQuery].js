632371e942ab4e95996194f9bd8ca4a7ce76b67c
fguillen1215 <111141844+fguillen1215@users.noreply.github.com>
2022-12-08 14:56:52 -0500

    added new searhquery algo

--- pages/api/recommendations/[searchQuery].js
+++ pages/api/recommendations/[searchQuery].js
@@ -1,23 +1,81 @@
+// const axios = require("axios")
+// const zlib = require("zlib");
+
+//   export default async function searchHandler(req, res) {
+//     const {
+//       query: {searchQuery},
+//       method,
+//     } = req;
+
+//     let searchItem
+//     if(typeof searchQuery === "string" || searchQuery.category === "all"){
+//       searchItem = searchQuery
+//     } else {
+//       for(let keys in searchQuery){
+//         searchItem = []
+//         searchItem.push(searchQuery[keys])
+//         searchItem.join(" ")
+//       }
+//     }
+
+//     switch (method) {
+//       case "GET":
+//         //GET PRODUCT RESULTS
+//         const options = {
+//           method: "GET",
+//           url: "https://api.scaleserp.com/search",
+//           responseType: "arraybuffer", //Important
+//           params: {
+//             api_key: process.env.API_KEY,
+//             q: searchItem,
+//             search_type: "shopping",
+//             location: "United States",
+//             sort_by: "review_score",
+//             max_page: 1,
+//           },
+//         };
+//         axios
+//           .request(options)
+//           .then(function (response) {
+//             console.log(response)
+//             zlib.gunzip(response.data, function (_err, output) {
+//               console.log(response)
+//               res.send(output);
+//             });
+//           })
+//           .catch(function (error) {
+//             console.error(error);
+//           });
+
+//         break;
+
+//       default:
+//         res.setHeader("Allow", ["GET"]);
+//         res.status(405).end(`Method ${method} Not Allowed`);
+//     }
+//   }
+
+
 const axios = require("axios")
 const zlib = require("zlib");
-
   export default async function searchHandler(req, res) {
     const {
-      query: {searchQuery},
+      query, //{pid:, query: query:}
       method,
     } = req;
-
     let searchItem
-    if(typeof searchQuery === "string" || searchQuery.category === "all"){
-      searchItem = searchQuery
-    } else {
-      for(let keys in searchQuery){
-        searchItem = []
-        searchItem.push(searchQuery[keys])
-        searchItem.join(" ")
+    console.log("QUERY", query)
+    if(Object.keys(query).length === 1 || query.category === "all"){
+      if(query.category){
+        searchItem = query.value
+      } else {
+        searchItem = query.searchQuery
       }
+      console.log("HIT NO FILTER", searchItem)
+    } else {
+      searchItem = query.category + " " + query.value
+      console.log("HIT FILTER", searchItem)
     }
-
     switch (method) {
       case "GET":
         //GET PRODUCT RESULTS
@@ -37,18 +95,17 @@ const zlib = require("zlib");
         axios
           .request(options)
           .then(function (response) {
-            console.log(response)
+            console.log(response.data)
             zlib.gunzip(response.data, function (_err, output) {
-              console.log(response)
-              res.send(output);
+              console.log(_err)
+              console.log(output)
+              res.send(output)
             });
           })
           .catch(function (error) {
             console.error(error);
           });
-
         break;
-
       default:
         res.setHeader("Allow", ["GET"]);
         res.status(405).end(`Method ${method} Not Allowed`);
