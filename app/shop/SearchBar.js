3039ed2feb7377101725c61a091f8a18769dc0b2
Alex Sun <alexsun424@gmail.com>
2022-12-12 22:35:17 -0500

    some shop page fixes

--- app/shop/SearchBar.js
+++ app/shop/SearchBar.js
@@ -1,40 +1,74 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import React, { useState, useEffect, useRef } from "react";
+import { useRouter, usePathname } from "next/navigation";
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchRecipients } from '../../store/recipientSlice'
 import { searchOff, deleteFilters, resetFilterType, resetChecklist, filterOff } from '../../store/shopSlice'
 
 function SearchBar() {
-  const recipients = useSelector((state) => state.recipients)
-  const userId = useSelector((state) => state.user)
+  const {recipients, singleRecipient} = useSelector((state) => state.recipients)
+  const router = useRouter();
+  const updatedRecip = useRef(false);
+  const updatedList = useRef(false)
+  const {userId} = useSelector((state) => state.user)
   const dispatch = useDispatch()
+  const path = usePathname()
 
-  const [currentRecipient, setRecipient] = useState(recipients.singleRecipient)
+  const [currentRecipient, setRecipient] = useState(null)
+  const [updatedRecipients, setUpdatedRecipients] = useState(null)
+
+  console.log("THE RECICPIENTS", updatedRecipients)
 
   useEffect(() => {
-    if(Object.keys(recipients.singleRecipient).length === 1 && Object.keys(currentRecipient).length === 1){
       dispatch(fetchRecipients(userId))
-    }
   }, [])
 
   useEffect(() => {
-  if(recipients.recipients.length !== 0 && Object.keys(currentRecipient).length === 1){
-    const currentRecipient = recipients.recipients[0]
-    const iterable = {}
-    Object.assign(iterable, currentRecipient)
-    let score = [...iterable.recommendations]
-    score = score.sort((a,b) => b.score - a.score).slice(0,5)
-    iterable.recommendations = score
-    setRecipient(iterable)
-  }
+    if(updatedRecip.current){
+      console.log("USE FFECT1")
+        updatedList.current = true
+        const recipient = path.split("/")[2]
+        const iterable = {}
+        // console.log("RECIPIENT IS", recipient)
+        if(recipient){
+          const prevRecipient = recipients.find(recipient => recipient.name === recipient)
+          Object.assign(iterable, prevRecipient)
+        } else {
+          if(singleRecipient.preferences.length !== 0){
+            Object.assign(iterable, singleRecipient)
+          } else {
+            Object.assign(iterable, recipients[0])
+          }
+        }
+        let score = [...iterable.recommendations]
+        score = score.sort((a,b) => b.score - a.score).slice(0,5)
+        iterable.recommendations = score
+        setRecipient(iterable)
+        setUpdatedRecipients(recipients)
+        if(!recipient){
+          router.push(`/shop/${iterable.name}/toprecs`)
+        }  
+    } else {
+      updatedRecip.current = true
+    }
   }, [recipients])
 
-  const [searchText, setSearchText] = useState("");
-  const router = useRouter();
+  useEffect(() => {
+    if(updatedList.current){
+      if(currentRecipient){
+        console.log("USE FFECT2", currentRecipient, updatedRecipients)
+        if(currentRecipient.name !== updatedRecipients[0].name){
+          const shownRecipients = []
+          shownRecipients.push(currentRecipient)
+          shownRecipients.push(...updatedRecipients.filter(recipients => recipients.name !== currentRecipient.name))
+          setUpdatedRecipients(shownRecipients)
+        }
+      }
+    }
+  }, [currentRecipient])
 
   const handleRecipient = (event) => {
-    const newRecipient = recipients.recipients.find(recipient => recipient.name === event.target.value)
+    const newRecipient = recipients.find(recipient => recipient.name === event.target.value)
     const iterable = {}
     Object.assign(iterable, newRecipient)
     const score = [...newRecipient.recommendations].sort((a,b) => b.score - a.score).slice(0,5)
@@ -45,7 +79,7 @@ function SearchBar() {
     dispatch(resetChecklist())
     dispatch(resetFilterType())
     dispatch(filterOff())
-    router.push(`/shop/${iterable.name}/TopRecs`)
+    router.push(`/shop/${iterable.name}/toprecs`)
   }
 
   const handleCategory = (event) =>{
@@ -58,15 +92,12 @@ function SearchBar() {
     else router.push(`/shop/${currentRecipient.name}/${event.target.id}/1`)
   }
 
-
-
-// console.log(currentRecipient, Object.keys(currentRecipient).length)
 return (
   <div className="flex flex-col h-36 rounded-lg bg-orange-200 justify-evenly">
     <div className="flex flex-row justify-evenly">
       <div>
        <select onChange={handleRecipient} className="h-8 w-32 rounded-lg text-center">
-          {recipients.recipients.map((recipient, index) => (<option key={index} value={recipient.name}> {recipient.name} </option>))}
+          {!updatedRecipients ? "" : updatedRecipients.map((recipient, index) => (<option key={index} value={recipient.name}> {recipient.name} </option>))}
        </select>
       </div>
       <div className="flex flex-col justify-center h-8 w-32 border-2 border-black rounded-lg">
@@ -74,7 +105,7 @@ return (
         Top Choices
        </div>
       </div>
-      { Object.keys(currentRecipient).length !== 1 ?
+      { currentRecipient ?
           currentRecipient.recommendations.map((recommendation, index) => (
               <div key={index} onClick={handleCategory} className="flex flex-col justify-center h-8 w-32 rounded-lg border-2 border-black">
                 <div id={recommendation.columnName} className="text-center"> {recommendation.columnName} </div>
